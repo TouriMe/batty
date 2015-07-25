908611017b839a7c667769f63ed347089d9d4b80
Song Yangyu <flyfy1@gmail.com>
2015-07-25 14:17:19 +0800

    Getting driver model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,7 @@
 class Driver < ActiveRecord::Base
   obfuscate_id
+
+  has_many :languages, class_name: 'DriverLanguage'
+
+  validates_presence_of :first_name, :last_name, :description, :phone
 end
--- app/models/driver_language.rb
+++ app/models/driver_language.rb
@@ -1,3 +1,7 @@
 class DriverLanguage < ActiveRecord::Base
   enum proficiency: [:spoken, :written]
+
+  belongs_to :driver
+
+  validates_presence_of :language_code, :proficiency
 end

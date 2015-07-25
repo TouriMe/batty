d5e1c8135bf96f7ab734bb61defe994c39dc7c6f
Song Yangyu <flyfy1@gmail.com>
2015-07-25 22:03:30 +0800

    refactor admin logic; bug fix of attrs in form submission

--- app/admin/driver.rb
+++ app/admin/driver.rb
@@ -1,31 +1,29 @@
 ActiveAdmin.register Driver do
+  particulars = {first_name:nil, last_name: nil, nickname: nil, short_desc: nil, description: :text}
+  contacts = {phone:nil, email:nil, wechat_id: nil, whatsapp_id: nil, facebook_url: nil }
+  intros  = {avatar_url: nil, video_url: nil }
+
+  permit_params *(particulars.keys + contacts.keys + intros.keys), languages_attributes: [:language_code, :proficiency]
+
+  register_fields = Proc.new do |f, fields|
+    fields.each do |field, type|
+      if type
+        f.input field, as: type
+      else
+        f.input field
+      end
+    end
+  end
+
   form do |f|
-    f.inputs 'Particulars' do
-      f.input :first_name
-      f.input :last_name
+    f.inputs('Particulars'){ register_fields[f, particulars] }
+    f.inputs('Contacts'){ register_fields[f, contacts]}
+    f.inputs('Intro'){ register_fields[f, intros]}
 
-      f.input :nickname
-      f.input :short_desc
-      f.input :description, as: :text
-    end
-
-    f.inputs 'Contacts' do
-      f.input :phone
-      f.input :email
-      f.input :wechat_id
-      f.input :whatsapp_id
-      f.input :facebook_url
-    end
-
-    f.inputs 'Intro' do
-      f.input :avatar_url
-      f.input :video_url
-    end
-
-    f.inputs "Language" do
+    f.inputs 'Language' do
       f.has_many :languages, allow_destroy: true, new_record: true do |l|
         l.input :language_code, as: :select, collection: LanguageList::COMMON_LANGUAGES.map{|l| [l.name, l.iso_639_3]}
-        l.input :proficiency, as: :select, collection: DriverLanguage.proficiencies
+        l.input :proficiency, as: :select, collection: DriverLanguage.proficiencies.keys.to_a
       end
     end
 

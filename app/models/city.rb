27837a70a987f91b352087fe6e6169da5670a57f
Song Yangyu <flyfy1@gmail.com>
2015-08-06 17:45:08 +0800

    adding admins for cities & vehicles

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,5 @@
 class City < ActiveRecord::Base
   obfuscate_id
+
+  validates_presence_of :name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150725045246) do
+ActiveRecord::Schema.define(version: 20150806093759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,12 @@ ActiveRecord::Schema.define(version: 20150725045246) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "driver_languages", force: :cascade do |t|
     t.integer  "driver_id"
     t.string   "language_code"
@@ -76,6 +82,12 @@ ActiveRecord::Schema.define(version: 20150725045246) do
     t.string   "whatsapp_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

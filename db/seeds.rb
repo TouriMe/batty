6bd7a4a6a3f088248738fdb5c81c2e09a75cf730
Song Yangyu <flyfy1@gmail.com>
2015-08-06 17:50:24 +0800

    adding cities & buses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
 AdminUser.create(email:'admin@cambodiansmile.com', password: '11111111')
+
+['Siem Reap', 'Phnom Penh'].each do |city_name|
+  City.create(name: city_name)
+end
+
+['Mini Bus' , 'Mini Van', 'Tuk Tuk'].each do |vehicle_name|
+  Vehicle.create(name: vehicle_name)
+end

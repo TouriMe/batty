ac880433aa389404d1d39b0ad76912d5d2141271
Song Yangyu <flyfy1@gmail.com>
2015-09-05 21:29:12 +0800

    assign a default email if not present

--- app/models/tourist.rb
+++ app/models/tourist.rb
@@ -8,11 +8,11 @@ class Tourist < User
     return @tourist if @tourist
 
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
+      email = auth.info.email ? auth.info.email : "user-#{auth.uid}@#{auth.provider}.com"
+      user.email = email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       user.image = auth.info.image # assuming the user model has an image
-      puts "user_info: #{[user.email, user.password, user.name, user.image]}"
     end
   end
 end

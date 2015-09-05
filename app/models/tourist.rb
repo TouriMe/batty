9a48ad882f7402c884513a3ac1cc39a1a89dc1b0
Song Yangyu <flyfy1@gmail.com>
2015-09-05 20:55:36 +0800

    adding debug info

--- app/models/tourist.rb
+++ app/models/tourist.rb
@@ -12,6 +12,7 @@ class Tourist < User
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       user.image = auth.info.image # assuming the user model has an image
+      puts "user_info: #{[user.email, user.password, user.name, user.image]}"
     end
   end
 end

5a9cea045b5098ef9001ce0a0019e08e2063ab00
Song Yangyu <flyfy1@gmail.com>
2015-10-08 23:52:18 +0800

    add trip model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926065723) do
+ActiveRecord::Schema.define(version: 20151008154056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,23 @@ ActiveRecord::Schema.define(version: 20150926065723) do
     t.integer  "user_id"
   end
 
+  create_table "purchases", force: :cascade do |t|
+    t.integer  "purchasable_id"
+    t.string   "purchasable_type"
+    t.decimal  "price"
+    t.integer  "buyer_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.string   "name"
+    t.text     "content"
+    t.money    "price_cent", scale: 2
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -111,9 +128,13 @@ ActiveRecord::Schema.define(version: 20150926065723) do
     t.string   "uid"
     t.string   "name"
     t.string   "image"
+    t.string   "phone_number"
+    t.string   "country"
+    t.string   "city"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["phone_number"], name: "index_users_on_phone_number", using: :btree
   add_index "users", ["provider"], name: "index_users_on_provider", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["uid"], name: "index_users_on_uid", using: :btree

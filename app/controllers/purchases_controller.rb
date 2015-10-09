cee767ddb808336c7773c4c38d40c5229a9276c0
Song Yangyu <flyfy1@gmail.com>
2015-10-09 23:49:23 +0800

    template for payment done

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,5 +3,14 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    nonce = params[:payment_method_nonce]
+    result = Braintree::Transaction.sale(
+        :amount => "10.00",
+        :payment_method_nonce => nonce
+    )
+    flash[:success] = 'Payment made successfully'
+  end
+
+  def checkout
   end
 end

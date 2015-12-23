3d345b806c729befd9a6b5f81c74b9e83d1ee141
honglin <yanghl22@gmail.com>
2015-12-24 00:45:15 +0800

    Fix the payment calculation bug

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,53 +4,53 @@ class PurchasesController < ApplicationController
   #
   #
   ####
-  
+
   # Create a purchase record 
   # before making a transcations 
   def create()
     @purchase = Purchase.new(ajax_params)
     @purchase.save
 
-    if @purchase.save    
+    if @purchase.save
       render json: @purchase
-    else 
-      render json: { "message" => "Not succcess" }
-    end 
+    else
+      render json: {"message" => "Not succcess"}
+    end
   end
- 
+
 
   # book the tour(:tour_id)
   # with driver (:driver_id) 
   def new
     @vehicle_type = params[:vehicle_type]
-    @trip   = Trip.friendly.find(params[:tour_id])
-    @driver = Driver.friendly.find(params[:driver_id])  
+    @trip = Trip.friendly.find(params[:tour_id])
+    @driver = Driver.friendly.find(params[:driver_id])
     @purchase = Purchase.new
     @no_show_title = true
 
+    @charge = 0
     if @trip.down_payment
-      @charge = @trip.down_payment + @trip.booking_fee
-    elsif @trip.booking_fee
-      @charge = @trip.booking_fee
-    else
-      @charge = 0
+      @charge += @trip.down_payment
+    end
+    if @trip.booking_fee
+      @charge += @trip.booking_fee
     end
 
-    if @vehicle_type == "Tuk Tuk" 
-      @later_pay = (@trip.tuktuk_price_cents/100) - @charge  
-      @driver.vehicles.each do |v| 
+    if @vehicle_type == "Tuk Tuk"
+      @later_pay = (@trip.tuktuk_price_cents/100) + @trip.booking_fee - @charge
+      @driver.vehicles.each do |v|
         if v.name.downcase == "tuk tuk"
           @vehicle_id = v.id
         end
       end
-    else
-      @later_pay = (@trip.car_price_cents/100) - @charge
-      @driver.vehicles.each do |v| 
-        if v.name.downcase != "tuk tuk"
+    elsif @vehicle_type == "Car"
+      @later_pay = (@trip.car_price_cents/100) + @trip.booking_fee - @charge
+      @driver.vehicles.each do |v|
+        if v.name.downcase == "Car"
           @vehicle_id = v.id
         end
       end
-    end 
+    end
     @braintree_key = Braintree::ClientToken.generate
   end
 
@@ -60,21 +60,21 @@ class PurchasesController < ApplicationController
     @nonce = params[:payment_method_nonce]
     @purchase = Purchase.find(params[:id])
     @trip = Trip.find(@purchase.purchasable_id)
-    
+
     if @trip.down_payment && @trip.booking_fee
       @charge = @trip.down_payment + @trip.booking_fee
     else
       @charge = 0
     end
 
-    if (@charge > 0 )
+    if (@charge > 0)
       result = Braintree::Transaction.sale(
-        :amount => @charge,
-        :payment_method_nonce => @nonce,
-        :options => {
-          :submit_for_settlement => true
-        }
-      ) 
+          :amount => @charge,
+          :payment_method_nonce => @nonce,
+          :options => {
+              :submit_for_settlement => true
+          }
+      )
       if result
         @purchase.status = :paid
         @purchase.save
@@ -82,7 +82,7 @@ class PurchasesController < ApplicationController
       end
     end
   end
-  
+
   def edit
     if @purchase.status == :paid
       redirect_to request.referer
@@ -93,7 +93,7 @@ class PurchasesController < ApplicationController
   def success
     @custom_title = "Your payment has been successful"
   end
-  
+
   def update
     @purchase.status = :paid
 
@@ -120,13 +120,13 @@ class PurchasesController < ApplicationController
 
   private
   def ajax_params # params that needed for an ajax create request
-    params.require(:purchase).permit(:purchasable_id, :purchasable_type, :start_date, :email, 
-                                      :email_confirmation, :country_code, :phone_number, :comments, :driver_id, :vehicle_id)
+    params.require(:purchase).permit(:purchasable_id, :purchasable_type, :start_date, :email,
+                                     :email_confirmation, :country_code, :phone_number, :comments, :driver_id, :vehicle_id)
   end
 
   def purchase_params
     params.require(:purchase).permit(:purchasable_id, :purchasable_type, :start_date, :email, :driver_id,
-                                      :vehicle_id, :country, :email_confirmation, :country_code, :phone_number, :contact, :comments)
+                                     :vehicle_id, :country, :email_confirmation, :country_code, :phone_number, :contact, :comments)
   end
 
 end

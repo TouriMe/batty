0091229ce1d0353e37cdc3f77e749cbd0af8a807
talnet <tinaunglinn.eng91@gmail.com>
2015-12-26 11:13:21 +0630

    Added PURCHASE to admin dashboard

---
+++ app/admin/purchase.rb
@@ -0,0 +1,14 @@
+ActiveAdmin.register Purchase do
+  
+  index do
+    selectable_column
+    column :price
+    column :email
+    column :vehicle_id
+    column :driver_id   
+    column :purchasable_id
+    column :reference_id
+    column :status
+    column :start_date
+  end
+end

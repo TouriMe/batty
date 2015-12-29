ffa44abd30d3548c1df580bb12096124096bab64
talnet <tinaunglinn.eng91@gmail.com>
2015-12-29 13:25:15 +0630

    Rails already have static file check

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,6 @@ module CombodianSmile
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.middleware.use ActionDispatch::Flash
-    config.serve_static_files = true
+    config.middleware.use ActionDispatch::Flash 
   end
 end

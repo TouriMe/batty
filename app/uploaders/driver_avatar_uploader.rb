42015cb55f794622d8df86ad2ec081e3baad53ec
talnet <tinaunglinn.eng91@gmail.com>
2016-01-05 02:53:17 +0630

    Change Uploader on Driver_avatar

--- app/uploaders/driver_avatar_uploader.rb
+++ app/uploaders/driver_avatar_uploader.rb
@@ -9,13 +9,14 @@ class DriverAvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  version :processed do
-    process :resize_to_fill => [150, 150]
-  end
-  
   version :square do
     process :resize_to_fill => [150, 150]
   end
+
+  version :popup do
+    process :resize_to_fill => [300, 300]
+  end
+
 
   def extension_white_list
     %w(jpg jpeg gif png)

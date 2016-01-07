50bd1ae0435bcfe3ae5d830f5faf07a639269fcd
honglin <yanghl22@gmail.com>
2016-01-07 14:08:16 +0800

    Fix tye typo

--- spec/controllers/trips_controller_spec.rb
+++ spec/controllers/trips_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe TrousController, type: :controller do
+RSpec.describe ToursController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do

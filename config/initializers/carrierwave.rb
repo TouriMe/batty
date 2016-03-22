130392522f7a798194713200abc4304e8578c44d
honglin <yanghl22@gmail.com>
2016-03-23 00:36:18 +0800

    Change the image url to http

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key => ENV['S3_SECRET'],
     :region                => ENV['S3_REGION']
   }
-
+  config.fog_use_ssl_for_aws=false
   config.cache_dir = "#{Rails.root}/tmp/uploads"                
   config.fog_directory    = ENV['S3_BUCKET_NAME']
 end

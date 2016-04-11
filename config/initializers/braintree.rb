186e3384b5840900f5fffa9fc0f63eae2be59c63
honglin <yanghl22@gmail.com>
2016-04-11 22:46:41 +0800

    change to support@tourime.com braintree sanbox account

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,4 @@
 Braintree::Configuration.environment = :sandbox
-Braintree::Configuration.merchant_id = 'n5vhhq9njvkyvs7z'
-Braintree::Configuration.public_key = 'mnffryh9shb37w98'
-Braintree::Configuration.private_key = '22f09e72ce631a37a4545b8fe1b0a077'
+Braintree::Configuration.merchant_id = '2mn5qrcnnb6scm98'
+Braintree::Configuration.public_key = '93sf8ss6gw4kyqh2'
+Braintree::Configuration.private_key = '61396599441f03c9694b028ea3d2586b'

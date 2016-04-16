2ab010ea247ef329445a1027508656cd175e7082
honglin <yanghl22@gmail.com>
2016-04-16 18:43:22 +0800

    Refine style fore payment part

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,4 @@
 Braintree::Configuration.environment = :sandbox
-Braintree::Configuration.merchant_id = 'n5vhhq9njvkyvs7z'
-Braintree::Configuration.public_key = 'mnffryh9shb37w98'
-Braintree::Configuration.private_key = '22f09e72ce631a37a4545b8fe1b0a077'
+Braintree::Configuration.merchant_id = ENV['BRAINTREE_MERCHANT_ID']
+Braintree::Configuration.public_key = ENV['BRAINTREE_PUBLIC_KEY']
+Braintree::Configuration.private_key = ENV['BRAINTREE_PRIVATE_KEY']

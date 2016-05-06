53e9b96ca1221237b78e8c6ba8a70414b5853856
honglin <yanghl22@gmail.com>
2016-05-06 22:38:13 +0700

    Set default status to inacitve when create a new tour

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160501132305) do
+ActiveRecord::Schema.define(version: 20160506151607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -264,7 +264,7 @@ ActiveRecord::Schema.define(version: 20160501132305) do
     t.string   "tour_start"
     t.string   "tour_end"
     t.string   "card_img"
-    t.boolean  "is_active",             default: true
+    t.boolean  "is_active",             default: false
   end
 
   create_table "users", force: :cascade do |t|

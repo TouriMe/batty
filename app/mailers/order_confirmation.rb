ea87129c3868c806d1b85f660a3958e3a3ab87f3
honglin <yanghl22@gmail.com>
2016-06-29 23:07:04 +0800

    Fix price issue on email

--- app/mailers/order_confirmation.rb
+++ app/mailers/order_confirmation.rb
@@ -4,11 +4,6 @@ class OrderConfirmation < ApplicationMailer
   def confirm(purchase, hostname)
     @purchase = purchase
     @hostname = hostname
-    if @purchase.purchasable.tuktuk_price.to_i > 0
-      @charge = @purchase.purchasable.tuktuk_price.to_i + @purchase.purchasable.ticket_price_cents.to_i
-    else
-      @charge = @purchase.purchasable.car_price.to_i + @purchase.purchasable.ticket_price_cents.to_i
-    end
     puts 'purchase created.'
     mail(to: @purchase.email, subject: 'Your Booking with TouriMe has been confirmed')
   end

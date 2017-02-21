6b781449c4fd2cd2bf0f4837714b811084103445
kirykr <kirymeas@gmail.com>
2017-02-21 13:45:18 +0700

    Modified carrierwave receive aws instances

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,12 +4,12 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_credentials = {
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET'],
-      :region                => ENV['S3_REGION']
+      :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region                => ENV['AWS_REGION']
     }
     config.fog_use_ssl_for_aws=false
     config.cache_dir = "#{Rails.root}/tmp/uploads"                
-    config.fog_directory    = ENV['S3_BUCKET_NAME']
+    config.fog_directory    = ENV['AWS_BUCKET_NAME']
   end
 end

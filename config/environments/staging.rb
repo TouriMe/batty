98da6b9f3d0887320f12fee013655b0533e39648
kirykr <kirymeas@gmail.com>
2017-02-21 15:56:35 +0700

    Cheanged AWS asset name

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "//#{ENV['S3_BUCKET_NAME']}.s3.amazonaws.com"
+  config.action_controller.asset_host = "//#{ENV['AWS_BUCKET_NAME']}.s3.amazonaws.com"
   config.assets.prefix = "/assets"
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

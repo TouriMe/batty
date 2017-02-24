a2bae96ad37447ae2ca5f04618200d17447f2cb1
kirykr <kirymeas@gmail.com>
2017-02-24 16:08:27 +0700

    check if there is a vehicle

--- app/helpers/drivers_helper.rb
+++ app/helpers/drivers_helper.rb
@@ -5,24 +5,26 @@ module DriversHelper
   end
 
   def get_first_vehicle(driver)
-    driver.vehicles.first.name
+    if driver.vehicles.count > 0
+      driver.vehicles.first.name
+    end
   end
   #------------
 
   def driver_skills(driver)
-  	skills = 'Years Driving Experience'
-  	if driver.english_communication
-  		skills += ', English Communication'
-  	end
-  	if driver.basic_history
-  		skills += ', Basic History'
-  	end
-  	if driver.smartphone_photography
-  		skills += ', Smartphone Photography'
-  	end
-  	if driver.basic_dslr
-  		skills += ', Basic DSLR'
-  	end
-  	skills
+    skills = 'Years Driving Experience'
+    if driver.english_communication
+      skills += ', English Communication'
+    end
+    if driver.basic_history
+      skills += ', Basic History'
+    end
+    if driver.smartphone_photography
+      skills += ', Smartphone Photography'
+    end
+    if driver.basic_dslr
+      skills += ', Basic DSLR'
+    end
+    skills
   end
 end

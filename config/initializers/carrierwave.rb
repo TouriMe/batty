56869de24b7be967c41b332d0ed816e458571926
raizer123 <caili.cl29@gmail.com>
2017-03-09 17:01:52 +0700

    Modified configuration on carrierwave.rb file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,17 @@
-CarrierWave.configure do |config|
-  config.storage = :file
+require 'carrierwave/storage/fog'
 
- if Rails.env.staging? || Rails.env.production?
-    config.storage = 'fog'
-    config.cache_dir = "#{Rails.root}/tmp/uploads"
+CarrierWave.configure do |config| 
+  config.storage = :file
+  if Rails.env.staging? || Rails.env.production?
+    config.storage = :fog
     config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['FOG_REGION']
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region                => ENV['AWS_REGION']
     }
-    config.fog_directory = ENV['FOG_DIRECTORY']
+    config.fog_use_ssl_for_aws=false
+    config.cache_dir = "#{Rails.root}/tmp/uploads"                
+    config.fog_directory    = ENV['AWS_BUCKET_NAME']
   end
 end

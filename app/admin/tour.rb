f55a769b80f0ba6985553d3e700ef3f1d77490d7
raizer123 <caili.cl29@gmail.com>
2017-03-17 10:23:46 +0700

    add feature driver selection

--- app/admin/tour.rb
+++ app/admin/tour.rb
@@ -69,21 +69,21 @@ ActiveAdmin.register Tour do
       input :tour_end
     end
 
-    f.inputs 'Drivers' do
-      f.has_many :tour_drivers, allow_destroy: true do |driver|
-        driver.input :driver_id, label: 'Select Drivers', as: :select, collection: (Driver.all.map{|d| [d.full_name, d.id]}), prompt: 'All Drivers' 
-      end
-    end
+    # f.inputs 'Drivers' do
+    #   f.has_many :tour_drivers, allow_destroy: true do |driver|
+    #     driver.input :driver_id, label: 'Select Drivers', as: :select, collection: (Driver.all.map{|d| [d.full_name, d.id]}), prompt: 'All Drivers' 
+    #   end
+    # end
 
-    f.inputs 'length' do
-      f.input :length_id, as: :select, collection: (Length.all.map{|l| [l.length_title, l.id]}), prompt: 'Choose length'
-    end
+    # f.inputs 'length' do
+    #   f.input :length_id, as: :select, collection: (Length.all.map{|l| [l.length_title, l.id]}), prompt: 'Choose length'
+    # end
 
-    f.inputs 'Activities' do
-      f.has_many :tour_activity, allow_destroy: true do |activity|
-        activity.input :activity_id, label: 'Select Activities', as: :select, collection: (Activity.all.map{|a| [a.activity_name, a.id]}), prompt: 'Choose activities'
-      end
-    end
+    # f.inputs 'Activities' do
+    #   f.has_many :tour_activity, allow_destroy: true do |activity|
+    #     activity.input :activity_id, label: 'Select Activities', as: :select, collection: (Activity.all.map{|a| [a.activity_name, a.id]}), prompt: 'Choose activities'
+    #   end
+    # end
 
     f.inputs 'Price Calculation' do
       input :tuktuk_price

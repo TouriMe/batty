027851eaa06cc4a13296e8e4531577cb2f8b49c4
raizer123 <caili.cl29@gmail.com>
2017-05-03 15:40:12 +0700

    center dynamic activity

--- app/helpers/tours_helper.rb
+++ app/helpers/tours_helper.rb
@@ -6,4 +6,14 @@ module ToursHelper
 	def number_of_reviews(tour)
 		tour_reviews = Review.where(review_type: 'tour', name: "#{tour.name}").count
 	end
+
+	def number_of_tour_type(activity)
+		tour_type = []
+		activity.tours.map {|tour| tour_type << tour.tour_type}
+		if tour_type.uniq.count < 4
+			return 'text-center'
+		else
+			return ''
+		end
+	end
 end
